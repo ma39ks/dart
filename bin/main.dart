b5b2cb9bf4e54f51afafa7633d22b96ed284eae0
ma39ks <ma39ks@gmail.com>
2022-09-14 20:47:48 +0300

    first task

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,29 @@
 import 'dart:io';
+import 'dart:math';
+
+Future<String> getC (String char) async {
+
+  await Future.delayed(Duration(seconds: 2));
+  return char;
+}
 
 void main() async {
-  var dir = Directory('.');
+  var leng = ['а','б','в','г','д','е','ё','ж','з','и','к','л','м','н','о','п',
+    'р','с','т','у','ф','х','ч','ш','щ','ы','э','ю','я'];
+  var file = File('file.txt');
+  var sink = file.openWrite();
+  var rng = Random();
+
   try {
-    var dirList = dir.list();
-    await for (final FileSystemEntity f in dirList) {
-      if (f is File) {
-        print('Found file ${f.path}');
-      } else if (f is Directory) {
-        print('Found dir ${f.path}');
-      }
+    for (var i = 0; i < 21; i++) {
+      var tmp = await getC(leng[rng.nextInt(leng.length)]);
+      print(tmp);
+      sink.write(tmp);
+      print(file);
     }
   } catch (e) {
+    sink.close();
     print(e.toString());
   }
+  sink.close();
 }
